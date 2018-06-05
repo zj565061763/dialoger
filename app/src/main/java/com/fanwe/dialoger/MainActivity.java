0088ae380ec16fb295decd27a541907bab97ba38
zjun <565061763@qq.com>
2018-06-05 16:50:07 +0800

    demo

--- app/src/main/java/com/fanwe/dialoger/MainActivity.java
+++ app/src/main/java/com/fanwe/dialoger/MainActivity.java
@@ -4,27 +4,38 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 
 import com.fanwe.lib.dialoger.Dialoger;
+import com.fanwe.lib.dialoger.impl.FDialoger;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
     public static final String TAG = MainActivity.class.getSimpleName();
-
-    private TestDialoger mDialoger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showSimpleDemo();
+    }
 
-        mDialoger = new TestDialoger(this);
+    private void showSimpleDemo()
+    {
+        final FDialoger dialoger = new FDialoger(this);
 
-        // 设置窗口关闭监听
-        mDialoger.setOnDismissListener(new Dialoger.OnDismissListener()
+        /**
+         * 设置窗口要显示的内容
+         */
+        dialoger.setContentView(new Button(this));
+
+        /**
+         * 设置窗口关闭监听
+         */
+        dialoger.setOnDismissListener(new Dialoger.OnDismissListener()
         {
             @Override
             public void onDismiss(Dialoger dialoger)
@@ -33,8 +44,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        // 设置窗口显示监听
-        mDialoger.setOnShowListener(new Dialoger.OnShowListener()
+        /**
+         * 设置窗口显示监听
+         */
+        dialoger.setOnShowListener(new Dialoger.OnShowListener()
         {
             @Override
             public void onShow(Dialoger dialoger)
@@ -43,14 +56,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        // 设置按返回键是否可以关闭窗口，默认true
-        mDialoger.setCancelable(true);
+        /**
+         * 设置按返回键是否可以关闭窗口，默认true
+         */
+        dialoger.setCancelable(true);
 
-        // 设置触摸到非内容区域是否关闭窗口，默认false
-        mDialoger.setCanceledOnTouchOutside(true);
+        /**
+         * 设置触摸到非内容区域是否关闭窗口，默认false
+         */
+        dialoger.setCanceledOnTouchOutside(true);
 
-        // 设置窗口背景颜色，默认#66000000
-        mDialoger.setBackgroundColor(Color.parseColor("#66000000"));
+        /**
+         * 设置窗口背景颜色，默认#66000000
+         */
+        dialoger.setBackgroundColor(Color.parseColor("#66000000"));
+
+        /**
+         * 设置上下左右间距
+         */
+        dialoger.setPadding(0, 0, 0, 0);
+
+        /**
+         * 设置重力属性
+         */
+        dialoger.setGravity(Gravity.CENTER);
+
+        /**
+         * 显示窗口
+         */
+        dialoger.show();
     }
 
     @Override
@@ -62,16 +96,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 new PositionDialoger(this, v).show();
                 break;
         }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)
-    {
-        /**
-         * 传递按键事件给窗口
-         */
-        if (mDialoger.onKeyDown(keyCode, event))
-            return true;
-        return super.onKeyDown(keyCode, event);
     }
 }
