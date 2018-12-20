3e33ec2b5723cf52212d6620e44bced512c9f022
zjun <565061763@qq.com>
2018-12-20 19:07:15 +0800

    SlideBottomTopCreater

--- lib/src/main/java/com/sd/lib/dialoger/animator/SlideBottomTopCreater.java
+++ lib/src/main/java/com/sd/lib/dialoger/animator/SlideBottomTopCreater.java
@@ -7,16 +7,6 @@ import android.view.View;
  */
 public class SlideBottomTopCreater extends SlideVerticalCreater
 {
-    private float mValueOriginal;
-
-    @Override
-    protected void beforeCreateAnimator(boolean show, View view)
-    {
-        super.beforeCreateAnimator(show, view);
-        if (show)
-            mValueOriginal = view.getTranslationY();
-    }
-
     @Override
     protected float getValueHidden(View view)
     {
@@ -26,14 +16,6 @@ public class SlideBottomTopCreater extends SlideVerticalCreater
     @Override
     protected float getValueShown(View view)
     {
-        return mValueOriginal;
-    }
-
-    @Override
-    protected void onAnimationEnd(boolean show, View view)
-    {
-        super.onAnimationEnd(show, view);
-        if (!show)
-            view.setTranslationY(mValueOriginal);
+        return 0;
     }
 }
