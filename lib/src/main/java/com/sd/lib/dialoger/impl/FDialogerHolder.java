d3cfe53671576c3c095e0f54539bbaba5cb78f58
zjun <565061763@qq.com>
2020-09-29 17:23:11 +0800

    remove ActivityConfig

--- lib/src/main/java/com/sd/lib/dialoger/impl/FDialogerHolder.java
+++ lib/src/main/java/com/sd/lib/dialoger/impl/FDialogerHolder.java
@@ -2,19 +2,16 @@ package com.sd.lib.dialoger.impl;
 
 import android.app.Activity;
 
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.WeakHashMap;
 
-public class FDialogerHolder
+class FDialogerHolder
 {
     private static final Map<Activity, Collection<FDialoger>> MAP_ACTIVITY_DIALOG = new HashMap<>();
-    private static final Map<Activity, ActivityConfig> MAP_ACTIVITY_CONFIG = new WeakHashMap<>();
 
     static synchronized void addDialoger(FDialoger dialoger)
     {
@@ -42,7 +39,6 @@ public class FDialogerHolder
         if (holder.isEmpty())
         {
             MAP_ACTIVITY_DIALOG.remove(activity);
-            restoreActivityConfig(activity);
         }
     }
 
@@ -58,64 +54,5 @@ public class FDialogerHolder
     static synchronized void remove(Activity activity)
     {
         MAP_ACTIVITY_DIALOG.remove(activity);
-        MAP_ACTIVITY_CONFIG.remove(activity);
-    }
-
-    private static void restoreActivityConfig(Activity activity)
-    {
-        final ActivityConfig config = MAP_ACTIVITY_CONFIG.get(activity);
-        if (config != null)
-            config.restore();
-    }
-
-    /**
-     * 返回activity对象对应的配置
-     *
-     * @param activity
-     * @return
-     */
-    public static synchronized ActivityConfig getActivityConfig(Activity activity)
-    {
-        if (activity == null)
-            return null;
-
-        ActivityConfig config = MAP_ACTIVITY_CONFIG.get(activity);
-        if (config == null)
-        {
-            config = new ActivityConfig(activity);
-            if (!activity.isFinishing())
-                MAP_ACTIVITY_CONFIG.put(activity, config);
-        }
-        return config;
-    }
-
-    public static final class ActivityConfig
-    {
-        private final WeakReference<Activity> mActivity;
-        private Integer mSystemUiVisibility;
-
-        private ActivityConfig(Activity activity)
-        {
-            mActivity = new WeakReference<>(activity);
-        }
-
-        public void save()
-        {
-            final Activity activity = mActivity.get();
-            if (activity == null)
-                return;
-
-            mSystemUiVisibility = activity.getWindow().getDecorView().getSystemUiVisibility();
-        }
-
-        private void restore()
-        {
-            final Activity activity = mActivity.get();
-            if (activity == null)
-                return;
-
-            if (mSystemUiVisibility != null)
-                activity.getWindow().getDecorView().setSystemUiVisibility(mSystemUiVisibility);
-        }
     }
 }
